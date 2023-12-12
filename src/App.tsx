5bb2eb58240cb2655214b4afd442ee00cc23914e
xuj16 <xuj16@mingyuanyun.com>
2023-12-12 14:03:23 +0800

    feat: 新增px转rem

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,18 @@ import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import router from './const/route';
-// import Layout from './layout';
+import Layout from './layout';
 
 function App() {
   return (
     <BrowserRouter>
-      {/* <Layout> */}
+      <Layout>
         <Routes>
           {router.map((route) => (
             <Route key={route.path} {...route} />
           ))}
         </Routes>
-      {/* </Layout> */}
+      </Layout>
     </BrowserRouter>
   );
 }
